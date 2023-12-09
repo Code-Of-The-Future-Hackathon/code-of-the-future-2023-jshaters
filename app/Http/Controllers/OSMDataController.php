4e8c1284260ea558b703000c9a0a9a95a3c7e175
trephyyy <trephyyy@gmail.com>
2023-12-09 16:32:15 +0200

    added API for greenspaces and merged them | NEED TO FIX NAMES AND LEISURE!!!

--- app/Http/Controllers/OSMDataController.php
+++ app/Http/Controllers/OSMDataController.php
@@ -2,38 +2,35 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\GreenSpace;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Http;
 use Inertia\Inertia;
 use Inertia\Response;
 
+class Cluster
+{
+    public $points = [];
+    public function addPoint($point)
+    {
+        $this->points[] = $point;
+    }
+}
 class OSMDataController extends Controller
 {
+
     public function index()
     {
-        $response = Http::asForm()->post('http://overpass-api.de/api/interpreter', [
-            'data' => '
- 
-        [out:json];
-(
-  way["leisure"="park"](22.3805257504, 41.2344859889, 28.5580814959, 44.2349230007);
-  way["leisure"="garden"](22.3805257504, 41.2344859889, 28.5580814959, 44.2349230007);
-  way["landuse"="grass"](22.3805257504, 41.2344859889, 28.5580814959, 44.2349230007);
-  way["landuse"="meadow"](22.3805257504, 41.2344859889, 28.5580814959, 44.2349230007);
-  way["landuse"="forest"](22.3805257504, 41.2344859889, 28.5580814959, 44.2349230007);
-);
-out geom;
->;
-    '
-        ]);
 
-        $data = $response->json();
-        $result = $data['elements'];
+
+        $data = GreenSpace::all();
+
+
 
 
 
         return Inertia::render('GreenSpacesMap', [
-            'greenSpaces' => $result,
+            'greenSpaces' => $data,
         ]);
     }
 }
